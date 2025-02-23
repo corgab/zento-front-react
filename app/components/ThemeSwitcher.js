8519ff68dccb1ccc2baf93f4f115c458408a22cf
corgab <corbanigabriele@gmail.com>
2025-02-23 23:02:29 +0100

    scaffolding

---
+++ app/components/ThemeSwitcher.js
@@ -0,0 +1,48 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export default function ThemeSwitcher() {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    // Controlla il tema salvato
+    const savedTheme = localStorage.getItem('theme');
+
+    // Se esiste un tema salvato
+    if (savedTheme) {
+      setTheme(savedTheme);
+      document.documentElement.classList.toggle('dark', savedTheme === 'dark');
+    } else {
+      // Se non c'è un tema salvato
+      const prefersDarkMode = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+      ).matches;
+      if (prefersDarkMode) {
+        setTheme('dark');
+        document.documentElement.classList.add('dark');
+      }
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+
+    setTheme(newTheme);
+
+    // Nuovo tema
+    localStorage.setItem('theme', newTheme);
+
+    // Applica o rimuovi la classe dark sull'elemento html
+    document.documentElement.classList.toggle('dark', newTheme === 'dark');
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className='p-2 rounded-full '
+    >
+      {theme === 'light' ? <h1>Light</h1> : <h1>Dark</h1>}
+    </button>
+  );
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+/* PurgeCSS per rimuovere le classi non utilizzate */
 export default {
+  darkMode: 'class',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    fontFamily: {
+      kumbh: ['var(--font-kumbh-sans)', 'sans-serif'],
+      inter: ['var(--font-inter)', 'serif'],
+    },
+    colors: {
+      yellow: '#FFD11A',
+      dark: {
+        8: '#141414',
+        10: '#1A1A1A',
+        15: '#262626',
+        20: '#333333',
+        25: '#404040',
+        30: '#4D4D4D',
+        35: '#595959',
+        40: '#666666',
+      },
+      gray: {
+        50: '#7E7E81',
+        60: '#98989A',
+        70: '#B3B3B3',
+        80: '#CCCCCC',
+        90: '#E4E4E7',
+        95: '#F1F1F3',
+        97: '#F7F7F8',
+        99: '#FCFCFD',
       },
     },
   },
